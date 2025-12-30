9fe576cb69c485ee087d577d35df854c65d53479
Negri234279 <rafalejo01@gmail.com>
2025-12-30 01:36:26 +0100

    feat: agregar soporte para exchange en RabbitConsumer y mejorar manejo de mensajes

--- src/providers/RabbitConsumer.js
+++ src/providers/RabbitConsumer.js
@@ -1,9 +1,10 @@
 import amqp from 'amqplib'
 
 export class RabbitConsumer {
-    constructor({ url, queue, logger, handler }) {
+    constructor({ url, queue, exchange = null, logger, handler = null }) {
         this.url = url
         this.queue = queue
+        this.exchange = exchange
         this.logger = logger
         this.handler = handler
 
@@ -12,31 +13,64 @@ export class RabbitConsumer {
         this.running = false
     }
 
-    async start() {
+    async connect() {
         this.logger.info(`Conectando a RabbitMQ en ${this.url}`)
 
         this.connection = await amqp.connect(this.url)
-        this.channel = await this.connection.createChannel()
+        this.channel = await this.connection.createConfirmChannel()
 
-        await this.channel.assertQueue(this.queue, { durable: true })
-        await this.channel.prefetch(1)
+        if (this.exchange) {
+            await this.channel.assertExchange(this.exchange, 'topic', { durable: true })
+        }
 
-        this.running = true
+        if (this.queue) {
+            await this.channel.assertQueue(this.queue, { durable: true })
+        }
+    }
 
-        this.logger.info(`Escuchando "${this.queue}"`)
+    async consume() {
+        if (!this.channel) throw new Error('No conectado')
 
-        this.channel.consume(this.queue, async (msg) => {
-            if (!msg || !this.running) return
+        this.logger.info(`Consumiento desde "${this.queue}"`)
+        this.consuming = true
+
+        await this.channel.consume(this.queue, async (msg) => {
+            if (!msg || !this.consuming) return
 
             try {
                 const payload = JSON.parse(msg.content.toString())
-                await this.handler.handle(payload)
+                await this.handler?.handle(payload)
                 this.channel.ack(msg)
             } catch (err) {
-                this.logger.error(err.message)
+                this.logger.error(err)
                 this.channel.nack(msg, false, false)
             }
         })
+    }
+
+    async sendToQueue(message) {
+        if (!this.channel) throw new Error('No conectado')
+
+        const buffer = Buffer.from(JSON.stringify(message))
+
+        return new Promise((resolve, reject) => {
+            this.channel.sendToQueue(this.queue, buffer, { persistent: true, timestamp: Date.now() }, (err, ok) => {
+                if (err) {
+                    this.logger.error(`Error al enviar mensaje a la queue "${this.queue}": ${err.message}`)
+                    return reject(err)
+                }
+
+                this.logger.info(`Mensaje enviado a la queue "${this.queue}": ${JSON.stringify(message)}`)
+                resolve(ok)
+            })
+        })
+    }
+
+    async publish(routingKey, message) {
+        if (!this.channel) throw new Error('No conectado')
+
+        const buffer = Buffer.from(JSON.stringify(message))
+        this.channel.publish(this.exchange, routingKey, buffer, { persistent: true })
     }
 
     async stop() {
